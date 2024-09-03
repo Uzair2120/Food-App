43b2d0893b876c51d27e3cb1f6410ecd0fcc0885
Uzair2120 <139527939+huzzi12@users.noreply.github.com>
2024-09-03 18:48:26 +0500

    started

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+
+class HomePage extends StatefulWidget{
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Home Page'),
+      ),
+    );
+  }
+}
